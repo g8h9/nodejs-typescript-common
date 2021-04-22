bc64d7f4127d3bac77eb292c72cc84c106cb0716
Giang Hoàng <gianghoang107@gmail.com>
2021-04-22 21:11:26 +0700

    chore: Add sort and mapper for query builder

--- src/lib/query-builder/query-builder.test.ts
+++ src/lib/query-builder/query-builder.test.ts
@@ -1,115 +1,130 @@
-import { Filter, QueryBuilder } from './index';
+import { QueryBuilder } from './index';
 
 it('query-builder test is null', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {is_null:null} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $eq: null } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { is_null: null } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $eq: null } }, map: [], sort: '' });
 });
 
 it('query-builder test not null', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {not_null:null} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $ne: null } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { not_null: null } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $ne: null } }, map: [], sort: '' });
 });
 
 it('query-builder test string equal', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {str_eq:'abc'} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $eq: 'abc' } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { str_eq: 'abc' } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $eq: 'abc' } }, map: [], sort: '' });
 });
 
 it('query-builder test string not equal', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {n_str_eq:'abc'} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $ne: 'abc' } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { n_str_eq: 'abc' } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $ne: 'abc' } }, map: [], sort: '' });
 });
 
 it('query-builder test string in', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {str_in:['abc','def','ghk']} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $in: ['abc', 'def', 'ghk'] } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { str_in: ['abc', 'def', 'ghk'] } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $in: ['abc', 'def', 'ghk'] } }, map: [], sort: '' });
 });
 
 it('query-builder test string not in', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {n_str_in:['abc', 'def', 'ghk']} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $nin: ['abc', 'def', 'ghk'] } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { n_str_in: ['abc', 'def', 'ghk'] } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $nin: ['abc', 'def', 'ghk'] } }, map: [], sort: '' });
 });
 
 it('query-builder test regex', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {regex:/^./g} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $regex: /^./g } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { regex: /^./g } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $regex: /^./g } }, map: [], sort: '' });
 });
 
 it('query-builder test number equal', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {num_eq:0} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $eq: 0 } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { num_eq: 0 } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $eq: 0 } }, map: [], sort: '' });
 });
 
 it('query-builder test number not equal', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {n_num_eq:0} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $ne: 0 } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { n_num_eq: 0 } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $ne: 0 } }, map: [], sort: '' });
 });
 
 it('query-builder test number greater than', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {gt:0} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $gt: 0 } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { gt: 0 } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $gt: 0 } }, map: [], sort: '' });
 });
 
 it('query-builder test number less than', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {lt:0} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $lt: 0 } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { lt: 0 } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $lt: 0 } }, map: [], sort: '' });
 });
 
 it('query-builder test number greater than or equal', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {gte:0} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $gte: 0 } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { gte: 0 } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $gte: 0 } }, map: [], sort: '' });
 });
 
 it('query-builder test number less than or equal', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {lte:0} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $lte: 0 } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { lte: 0 } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $lte: 0 } }, map: [], sort: '' });
 });
 
 it('query-builder test number between', () => {
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {num_between:[0,1]} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $gt: 0, $lt: 1 } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { num_between: [0, 1] } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $gt: 0, $lt: 1 } }, map: [], sort: '' });
 });
 
 it('query-builder test date equal', () => {
   const current: Date = new Date();
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {date_eq:current} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $eq: current } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { date_eq: current } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $eq: current } }, map: [], sort: '' });
 });
 
 it('query-builder test date not equal', () => {
   const current: Date = new Date();
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {n_date_eq:current} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $ne: current } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { n_date_eq: current } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $ne: current } }, map: [], sort: '' });
 });
 
 it('query-builder test date before', () => {
   const current: Date = new Date();
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {before:current} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $lte: current } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { before: current } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $lte: current } }, map: [], sort: '' });
 });
 
 it('query-builder test date after', () => {
   const current: Date = new Date();
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {after: current} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $gte: current } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { after: current } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $gte: current } }, map: [], sort: '' });
 });
 
 it('query-builder test date between', () => {
   const after: Date = new Date();
   const before: Date = new Date();
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {date_between: [after,before]} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $gte: after, $lte: before } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { date_between: [after, before] } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $gte: after, $lte: before } }, map: [], sort: '' });
 });
 
 it('query-builder test multiple operator on same field', () => {
-  const operator: Filter<string> = Filter.newOperator("str_eq", 'abc');
-  const operator2: Filter<string> = Filter.newOperator("n_str_eq", 'xyz');
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {str_eq:'abc',n_str_eq:'xyz'} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $eq: 'abc', $ne: 'xyz' } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ filter: { name: { str_eq: 'abc', n_str_eq: 'xyz' } } });
+  expect(queryBuilder.build()).toStrictEqual({ filter: { name: { $eq: 'abc', $ne: 'xyz' } }, map: [], sort: '' });
 });
 
 it('query-builder test multiple field', () => {
-  const operator: Filter<string> = Filter.newOperator("str_eq", 'abc');
-  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({ name: {str_eq:'abc'}, last_name: {str_eq:'abc'} });
-  expect(queryBuilder.build()).toStrictEqual({ name: { $eq: 'abc' }, last_name: { $eq: 'abc' } });
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({
+    filter: { name: { str_eq: 'abc' }, last_name: { str_eq: 'abc' } },
+  });
+  expect(queryBuilder.build()).toStrictEqual({
+    filter: { name: { $eq: 'abc' }, last_name: { $eq: 'abc' } },
+    map: [],
+    sort: '',
+  });
+});
+
+it('query-build test build map and sort', () => {
+  const queryBuilder: QueryBuilder = QueryBuilder.newBuilder({
+    filter: { name: { str_eq: 'abc' }, last_name: { str_eq: 'abc' } },
+    map: ['name', 'lastname'],
+    sort: ['+name', '-lastname'],
+  });
+  const expectResult = queryBuilder.build();
+  expect(expectResult.filter).toStrictEqual({ name: { $eq: 'abc' }, last_name: { $eq: 'abc' } });
+  expect(expectResult.map).toEqual(expect.arrayContaining(['lastname','name']));
+  expect(expectResult.sort).toStrictEqual('+name -lastname');
 });
--- src/lib/query-builder/query-builder.ts
+++ src/lib/query-builder/query-builder.ts
@@ -1,9 +1,12 @@
-import { Filters } from "./filter";
+import { Filters } from './filter';
 
 export abstract class QueryBuilder {
-  static newBuilder(filter: Record<string, Record<string,any>>): QueryBuilder {
-
-    return new MongoQueryBuilder(filter);
+  static newBuilder(query: {
+    filter: Record<string, Record<string, any>>,
+    sort?: string[],
+    map?: string[]}
+  ): QueryBuilder {
+    return new MongoQueryBuilder(query.filter, query.sort, query.map);
   }
 
   abstract build(): any;
@@ -11,19 +14,42 @@ export abstract class QueryBuilder {
 
 class MongoQueryBuilder extends QueryBuilder {
   filter: Record<string, Filters>;
+  sort: string[];
+  map: string[];
 
-  constructor(filter: Record<string, Record<string,any>>) {
+  constructor(filter: Record<string, Record<string, any>>, sort?: string[], map?: string[]) {
     super();
-    this.filter = Object.keys(filter).reduce((acc:Record<string,Filters>,crr:string)=> ({...acc,[crr]:Filters.parse(filter[crr])}),{});
+    this.filter = Object.keys(filter).reduce(
+      (acc: Record<string, Filters>, crr: string) => ({ ...acc, [crr]: Filters.parse(filter[crr]) }),
+      {},
+    );
+    this.sort = sort || [];
+    this.map = map || [];
   }
 
-  build(): Record<string, any> {
+  build(): Record<string,any> {
+    return {
+      filter: this.buildFilter(),
+      map: this.buildMap(),
+      sort: this.buildSort()
+    }
+  }
+
+  private buildFilter(): Record<string,any>{
     return (Object.keys(this.filter) as string[]).reduce(
       (acc: Record<string, any>, crr: string) => ({
         ...acc,
-        [crr]: this.filter[crr].query()
+        [crr]: this.filter[crr].query(),
       }),
       {},
     );
+  }
+
+  private buildSort(): string{
+    return this.sort.join(' ');
+  }
+
+  private buildMap(): string[]{
+    return this.map;
   }
 }
